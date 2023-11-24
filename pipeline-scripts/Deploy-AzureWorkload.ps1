91fe47b05a5183d97f872c81b4be5937b91ab4f9
Øystein Stræte <mail@qbits.no>
2023-11-24 08:45:39 +0100

    test: token

--- pipeline-scripts/Deploy-AzureWorkload.ps1
+++ pipeline-scripts/Deploy-AzureWorkload.ps1
@@ -9,13 +9,31 @@ param (
   [String]$TemplateParameterFile
 )
 
-# Parameters necessary for deployment
-$inputObject = @{
-  DeploymentName        = 'deploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
-  Location              = $Location
-  TemplateFile          = $TemplateFile
-  TemplateParameterFile = $TemplateParameterFile
-  Verbose               = $true
+## Parameters necessary for deployment
+#$inputObject = @{
+#  DeploymentName        = 'deploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+#  Location              = $Location
+#  TemplateFile          = $TemplateFile
+#  TemplateParameterFile = $TemplateParameterFile
+#  Verbose               = $true
+#}
+#
+#New-AzSubscriptionDeployment @inputObject
+
+# Function to get access token
+function Get-AzResourceManagerAccessToken {
+    $context = Get-AzContext
+    $resourceProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $client = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($resourceProfile)
+
+    $token = $($client.AcquireAccessToken(($context).Tenant.TenantId).AccessToken)
+
+    return $token
 }
 
-New-AzSubscriptionDeployment @inputObject
+# Use powershell to get an accestoken based on the currently signed in account/service principal
+$headers = @{
+    Authorization = "Bearer $(Get-AzResourceManagerAccessToken)"
+}
+
+$roleDefinitionId = (Get-AzRoleDefinition -Name $RoleName).Id
