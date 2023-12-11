4c192f3cc8a001fc8bcfa7e6c630602930a6da68
Øystein Stræte <mail@qbits.no>
2023-12-11 09:47:22 +0100

    add publish

--- pipeline-scripts/Deploy-AzureWorkload.ps1
+++ pipeline-scripts/Deploy-AzureWorkload.ps1
@@ -1,55 +1,34 @@
 param (
-#  [Parameter()]
-#  [String]$Location = "$($env:LOCATION)",
-#
-#  [Parameter()]
-#  [String]$TemplateFile,
-#
-#  [Parameter()]
-#  [String]$TemplateParameterFile
+  [Parameter()]
+  [String]$Location = "$($env:LOCATION)",
 
   [Parameter()]
-  [String]$Foo = "bar"
+  [String]$TemplateFile = "infra-as-code\inspection-checklist-reports\main.bicep",
+
+  [Parameter()]
+  [String]$TemplateParameterFile = "infra-as-code\inspection-checklist-reports\main.$($env:ENVIRONMENT).bicepparam",
+
+  [Parameter()]
+  [String]$ResourceGroupName = "$($env:AZURE_FUNCTIONAPP_RESOURCE_GROUP_NAME)"
 )
 
-## Parameters necessary for deployment
-#$inputObject = @{
-#  DeploymentName        = 'deploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
-#  Location              = $Location
-#  TemplateFile          = $TemplateFile
-#  TemplateParameterFile = $TemplateParameterFile
-#  Verbose               = $true
-#}
-#
-#New-AzSubscriptionDeployment @inputObject
+$resourceGroupInputObject = @{
+  Name     = $ResourceGroupName
+  Location = $Location
+  Verbose  = $true
+  Force    = $true
+}
 
-# Function to get access token
-#function Get-AzResourceManagerAccessToken {
-#    $context = Get-AzContext
-#    $resourceProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
-#    $client = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($resourceProfile)
-#
-#    $token = $($client.AcquireAccessToken(($context).Tenant.TenantId).AccessToken)
-#
-#    return $token
-#}
-#
-## Use powershell to get an accestoken based on the currently signed in account/service principal
-#$headers = @{
-#    Authorization = "Bearer $(Get-AzResourceManagerAccessToken)"
-#}
 
-Write-Verbose "get rg"
-Get-AzResourceGroup
+# Parameters necessary for function app deployment
+$functionAppInputObject = @{
+  DeploymentName        = 'deploy-icr{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+  Location              = $Location
+  TemplateFile          = $TemplateFile
+  TemplateParameterFile = $TemplateParameterFile
+  ResourceGroupName     = $ResourceGroupName
+  Verbose               = $true
+}
 
-Write-Verbose "get az key vault"
-Get-AzKeyVault
-
-Write-Verbose "get az front door"
-Get-AzFrontDoor
-
-Write-Verbose "Get role defs from code"
-$listAllAzRoleDefinitions = Get-AzRoleDefinition -Scope "/subscriptions/a83145a3-215b-44a4-9387-a540faaa58e9"
-
-Write-Verbose "Get role defs"
-$roleDefinitionId = (Get-AzRoleDefinition -Scope "/subscriptions/a83145a3-215b-44a4-9387-a540faaa58e9").Id
+New-AzResourceGroup @resourceGroupInputObject
+New-AzResourceGroupDeployment @functionAppInputObject
---
+++ pipeline-scripts/Publish-IcrProject.ps1
@@ -0,0 +1,15 @@
+param(
+    [Parameter()]
+    [String]$ARCHIVE_PATH
+    [Parameter()]
+    [String]$AZURE_FUNCTIONAPP_NAME = "$($env:AZURE_FUNCTIONAPP_NAME)",
+    [Parameter()]
+    [String]$AZURE_FUNCTIONAPP_RESOURCE_GROUP_NAME = "$($env:AZURE_FUNCTIONAPP_RESOURCE_GROUP_NAME)"
+)
+
+Publish-AzWebApp -ArchivePath $ARCHIVE_PATH `
+  -Type "zip" `
+  -Name $AZURE_FUNCTIONAPP_NAME `
+  -ResourceGroupName $AZURE_FUNCTIONAPP_RESOURCE_GROUP_NAME `
+  -Force `
+  -Verbose
